81bd4127ec318ae35f3825cd53d974648615bbdb
aung htet paing <apaing894@gmail.com>
2020-04-14 16:24:55 +0630

    creating components

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="about">
     <h1>Events Lists</h1>
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
-      >Showing Events
-    </router-link>
+    <EventCard />
   </div>
 </template>
+
+<script>
+import EventCard from "@/components/EventCard.vue";
+export default {
+  components: {
+    EventCard
+  }
+};
+</script>
