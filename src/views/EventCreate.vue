582d6e3cc2a1e450fa887d2b61359ae6b89f9a77
aung htet paing <apaing894@gmail.com>
2020-04-24 14:01:27 +0630

    mutation pt1

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,29 +1,113 @@
 <template>
   <div>
     <h1>Create Events</h1>
-    <p>{{ user.name }}</p>
-    <ul>
-      <li v-for="category in categories" :key="category">{{ category }}</li>
-    </ul>
-    <p>There are {{ catLength }} categories.</p>
-    <p>{{ getEventById(2) }} todos are done.</p>
-    <!-- <p>this is created by{{ userName }}</p> -->
+    <form @submit.prevent="createEvent">
+      <label>Select a category</label>
+      <select v-model="event.category">
+        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+      </select>
+
+      <h3>Name & describe your events</h3>
+      <div class="field">
+        <label>Title</label>
+        <input
+          v-model="event.title"
+          type="text"
+          placeholder="add a event title"
+        />
+      </div>
+
+      <div class="field">
+        <label>Description</label>
+        <input
+          v-model="event.description"
+          type="text"
+          placeholder="add a event description"
+        />
+      </div>
+
+      <h3>Where is your event?</h3>
+      <div class="field">
+        <label>Location</label>
+        <input
+          v-model="event.location"
+          type="text"
+          placeholder="add a event location"
+        />
+      </div>
+
+      <h3>When is your event?</h3>
+      <div class="field">
+        <label>Date</label>
+        <datepicker v-model="event.date" placeholder="Select date" />
+      </div>
+
+      <div class="field">
+        <label>Select a time</label>
+        <select v-model="event.time">
+          <option v-for="time in times" :key="time">{{ time }}</option>
+        </select>
+      </div>
+
+      <input type="submit" class="button -fill-gradient" value="Submit" />
+    </form>
   </div>
 </template>
 
 <script>
-import { mapState, mapGetters } from "vuex";
-
+import Datepicker from "vuejs-datepicker";
 export default {
-  computed: {
-    catLength() {
-      return this.$store.getters.catLength;
+  components: {
+    Datepicker
+  },
+  data() {
+    const times = [];
+    for (let i = 0; i <= 24; i++) {
+      times.push(i + ":00");
+    }
+    return {
+      times,
+      categories: this.$store.state.categories,
+      event: this.createFreshEventObject()
+    };
+  },
+  methods: {
+    createEvent() {
+      this.$store
+        .dispatch("createEvent", this.event)
+        .then(() => {
+          this.$router.push({
+            name: "event-show",
+            params: { id: this.event.id }
+          });
+          this.event = this.createFreshEventObject();
+        })
+        .catch(() => {
+          console.log("there was an error in creating event");
+        });
     },
-    ...mapGetters(["getEventById"]),
-    // doneTodos() {
-    //   return this.$store.getters.doneTodos;
-    // },
-    ...mapState(["user", "categories"])
+    createFreshEventObject() {
+      const user = this.$store.state.user;
+      const id = Math.floor(Math.random() * 10000000);
+      return {
+        id: id,
+        user: user,
+        category: "",
+        organizer: user,
+        title: "",
+        description: "",
+        location: "",
+        date: "",
+        time: "",
+        attendees: []
+      };
+    }
   }
 };
 </script>
+
+<style scoped>
+.field {
+  margin-bottom: 24px;
+}
+</style>
